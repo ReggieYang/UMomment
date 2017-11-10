867b293a01de0ec62b4ebf8d1c53e5e69d350a2b
ReggieYang <1937730499@qq.com>
2017-11-10 18:56:34 -0500

    ~

--- application.py
+++ application.py
@@ -5,7 +5,7 @@ import os
 from flask import Flask, request, render_template
 from flask_cors import CORS
 
-from logic.student_logic import login
+from logic.student_logic import login, update_student, follow, unfollow
 from dao.student_dao import find
 
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
@@ -13,27 +13,136 @@ application = Flask(__name__, template_folder=tmpl_dir)
 CORS(application)
 
 
+def args2dict(request_args, args):
+    res = {}
+    for arg in args:
+        res[arg] = request_args.form[arg]
+    return res
+
+
 @application.route('/')
 def home_page():
-    print(find(1))
     return render_template('login.html')
 
 
 @application.route('/student/login/', methods=['POST'])
-def student_login():
-    if request.method == "POST":
-        id = request.form['id']
-        password = request.form['password']
-        student = login(id, password)
-        print(id)
-        print(password)
-        if student is not None:
-            context = dict(data=student)
-            return render_template('user.html', **context)
+def login_student():
+    student_id = request.form['student_id']
+    password = request.form['password']
+    student = login(student_id, password)
+    if student is not None:
+        context = dict(data=student)
+        return render_template('user.html', **context)
 
-        else:
-            # return render_template('error.html')
-            return 'error'
+    else:
+        return render_template('error.html')
+
+
+@application.route('/student/update/', methods=['POST'])
+def update_st():
+    args = ['user_id', "nick_name", "avatar", "school_id", "since", "email", "password",
+            "introduction"]
+    student = args2dict(request, args)
+    update_student(student)
+    user_id = request.form['user_id']
+    password = request.form['password']
+    student = login(user_id, password)
+    if student is not None:
+        context = dict(data=student)
+        return render_template('user.html', **context)
+    else:
+        return render_template('error.html')
+
+
+@application.route('/student/follow/', methods=['POST'])
+def set_followship():
+    follower = request.form['follower_id']
+    following = request.form['following_id']
+    if request.form['unfollow'] == 1:
+        unfollow(follower, following)
+    else:
+        follow(follower, following)
+
+
+@application.route('/school/show/', methods=['POST'])
+def get_school():
+    return
+
+
+@application.route('/school/showStudent/', methods=['POST'])
+def get_student():
+    return
+
+
+@application.route('/group/create/', methods=['POST'])
+def create_group():
+    return
+
+
+@application.route('/group/join/', methods=['POST'])
+def join_group():
+    return
+
+
+@application.route('/group/leave/', methods=['POST'])
+def leave_group():
+    return
+
+
+@application.route('/trend/delete/', methods=['POST'])
+def delete_trend():
+    return
+
+
+@application.route('/trend/create/', methods=['POST'])
+def post_trend():
+    return
+
+
+@application.route('/trend/like/', methods=['POST'])
+def like_trend():
+    return
+
+
+@application.route('/trend/comment/', methods=['POST'])
+def comment_trend():
+    return
+
+
+@application.route('/trend/find/', methods=['POST'])
+def get_trend():
+    return
+
+
+@application.route('/moment/delete/', methods=['POST'])
+def delete_moment():
+    return
+
+
+@application.route('/moment/create/', methods=['POST'])
+def post_moment():
+    return
+
+
+@application.route('/moment/like/', methods=['POST'])
+def like_moment():
+    return
+
+
+@application.route('/moment/comment/', methods=['POST'])
+def comment_moment():
+    return
+
+
+@application.route('/moment/', methods=['POST'])
+def get_all_moment():
+    return render_template('moment.html')
+
+
+@application.route('/moment/find/', methods=['POST'])
+def get_moment():
+    # return render_template('moment.html')
+    return
 
 
 # @application.route('/search/<keyword>')
--- logic/student_logic.py
+++ logic/student_logic.py
@@ -4,12 +4,22 @@ from dao.student_dao import find
 from vo import Student
 
 
-
-
 def login(student_id, password):
-    student = find(id)
+    student = find(student_id)
 
     if student['password'] == password:
         return student
     else:
         return None
+
+
+def update_student(student):
+    return
+
+
+def follow(user_id, following_id):
+    return
+
+
+def unfollow(user_id, following_id):
+    return
