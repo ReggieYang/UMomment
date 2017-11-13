e11019e6854b0e83d04e05926a0fcc1a4b6248fe
ReggieYang <1937730499@qq.com>
2017-11-13 13:33:14 -0500

    Almost Done

--- application.py
+++ application.py
@@ -223,4 +223,31 @@ def get_moment_comment():
 
 
 if __name__ == '__main__':
-    application.run(host='127.0.0.1', port=6565, debug=True)
+    import click
+
+
+    @click.command()
+    @click.option('--debug', is_flag=True)
+    @click.option('--threaded', is_flag=True)
+    @click.argument('HOST', default='0.0.0.0')
+    @click.argument('PORT', default=8111, type=int)
+    def run(debug, threaded, host, port):
+        """
+        This function handles command line parameters.
+        Run the server using:
+
+            python server.py
+
+        Show the help text using:
+
+            python server.py --help
+
+        """
+
+        HOST, PORT = host, port
+        print("running on %s:%d" % (HOST, PORT))
+        application.run(host=HOST, port=PORT, debug=debug, threaded=threaded)
+        # application.run(host='127.0.0.1', port=6565, debug=True)
+
+
+    run()
